ba317dad822d2cf71bad972a9cbba2e47eded930
wangsp <845146913@qq.com>
2020-11-06 19:53:23 +0800

    init sleuth trace log

--- src/main/java/com/example/mqdemo/controller/ProviderController.java
+++ src/main/java/com/example/mqdemo/controller/ProviderController.java
@@ -1,5 +1,7 @@
 package com.example.mqdemo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,9 @@ import javax.annotation.Resource;
 @RestController
 @RequestMapping("/mq")
 public class ProviderController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Resource
     private RabbitTemplate rabbitTemplate;
 
@@ -21,12 +26,14 @@ public class ProviderController {
     @GetMapping
     public String send(@RequestParam("p") String param) {
         rabbitTemplate.convertAndSend(exchange, "", "test-fanout:"+param);
+        log.info(">>>>> fanout msg send success!");
         return param+", send success!";
     }
 
     @GetMapping("/topic")
     public String sendTopic(@RequestParam("p") String param) {
         rabbitTemplate.convertAndSend(exchange, "mq.topic", "test-topic:"+param);
+        log.error(">>>>> topic msg send success!");
         return param+", send success!";
     }
 }
